0163b8ad6028c1fd5b577010e71509dd4fad3c2c
John Elliott <johnelliott626@gmail.com>
2024-02-23 13:38:29 -0500

    functionality added to hash passwords

--- API/src/services/userService.js
+++ API/src/services/userService.js
@@ -1,9 +1,12 @@
-const queryDB = require('../services/db');
+const queryDB = require('./db');
+const hashPassword = require('../utils/hashPassword');
+const comparePasswords = require('../utils/comparePasswords');
 
-async function createUser(username, password, firstname, lastname) {
-  const query = 'INSERT INTO Users (username, password, firstname, lastname) VALUES ($1, $2, $3, $4)';
-  const { rows } = await queryDB(query, [username, password, firstname, lastname]);
-  return rows.length === 1;
+async function createUser(username, password, firstname, lastname) {    
+    const hashedPassword = await hashPassword(password);
+    const query = 'INSERT INTO Users (username, password, firstname, lastname) VALUES ($1, $2, $3, $4)';
+    const { rows } = await queryDB(query, [username, hashedPassword, firstname, lastname]);
+    return rows.length === 1;
 }
 
 async function checkIfUserExists(username) {
@@ -13,9 +16,14 @@ async function checkIfUserExists(username) {
 }
 
 async function authenticateUser(username, password) {
-    const query = 'SELECT * FROM Users WHERE Username = $1 AND Password = $2';
-    const { rows } = await queryDB(query, [username, password]);
-    return rows.length === 1;
+    const query = 'SELECT * FROM Users WHERE Username = $1';
+    const { rows } = await queryDB(query, [username]);
+    if (rows.length !== 1) {
+        return false;
+    }
+    const hashedPassword = rows[0].password;
+    const passwordsMatch = await comparePasswords(password, hashedPassword);
+    return passwordsMatch;
 }
 
 module.exports = {
