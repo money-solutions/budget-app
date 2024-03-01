55fe81cff93d46062e5a9a85644a26362d725ab6
John Elliott <johnelliott626@gmail.com>
2024-03-01 17:06:21 -0500

    logout button created

--- UI/src/app/dashboard/page.js
+++ UI/src/app/dashboard/page.js
@@ -1,95 +1,52 @@
-import Image from "next/image";
+'use client'
+
+import BasicTabs from "@/components/BasicTabs";
+import { Typography, Grid, Button } from "@mui/material";
 import styles from "./page.module.css";
+import axiosInstance from '../../config/axiosConfig';
+import Cookies from 'js-cookie';
+import { useRouter } from 'next/navigation'
+
+
 
 export default function Home() {
+  const router = useRouter();
+
+  const handleLogout = async () => {
+    const sessionID = Cookies.get('sessionID');
+    if (sessionID) {
+      try {
+        const response = await axiosInstance.post('/logout', { sessionID });
+        console.log('Response:', response.data);
+        Cookies.remove('sessionID');
+        router.push('/');
+        router.forward();
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    } else {
+      console.error('Session ID not found in cookie');
+    }
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Hello There, This will be the user's budget dashboard!&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <Grid>
+      <Grid container>
+        <Grid item xs={11}>
+          <Typography variant="h4" gutterBottom>
+            Welcome To Your Budget Dashboard!
+          </Typography>
+        </Grid>
+        <Grid item xs={1}>
+          <Button variant="outlined" className={styles.blackButton} onClick={handleLogout}>Logout</Button>
+        </Grid>
+      </Grid>
+      <Grid item>
+        <BasicTabs></BasicTabs>
+      </Grid>
+      
+      
+    </Grid>
+    
   );
 }
---
+++ UI/src/components/Signup.js
@@ -0,0 +1,116 @@
+'use client'
+
+import React, { useState, useEffect, use } from 'react';
+import axiosInstance from '../config/axiosConfig';
+import TextField from '@mui/material/TextField';
+import { Stack } from '@mui/material';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Cookies from 'js-cookie';
+import { useRouter } from 'next/navigation'
+
+
+export default function SignupForm() {
+  const router = useRouter();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [firstname, setFirstname] = useState('');
+  const [lastname, setLastname] = useState('');
+  const [allFieldsFilled, setAllFieldsFilled] = useState(false);
+  const [sessionID, setSessionID] = useState(Cookies.get('sessionID') || '');
+
+  const handleUsernameChange = (event) => {
+  setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleFirstnameChange = (event) => {
+    setFirstname(event.target.value);
+  };
+
+  const handleLastnameChange = (event) => {
+    setLastname(event.target.value);
+  };
+
+  useEffect(() => {
+    handleInputChange();
+  }, [username, password, firstname, lastname])
+
+  const handleSignUp = async () => {
+    try {
+      const response = await axiosInstance.post('/signup', {
+        username: username,
+        password: password,
+        firstname: firstname,
+        lastname: lastname,
+      });
+
+      const sessionID = response.headers['session-id'];
+      Cookies.set('sessionID', sessionID, { expires: 7 }); // Expires in 7 days
+      setSessionID(sessionID);
+
+      console.log('POST request successful:', response.data);
+      
+      
+      router.push('/dashboard');
+      router.forward();
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  const handleInputChange = () => {
+    // Check if all fields are filled
+    const fields = [username, password, firstname, lastname]
+    console.log(fields)
+    const allFilled = fields.every(val => val.trim() !== '');
+    setAllFieldsFilled(allFilled);
+  };
+
+
+  return (
+      <div>
+        <Stack spacing={2}>
+          <Typography variant="h6" gutterBottom>
+            Please Sign Up:
+          </Typography>
+          <TextField
+            required
+            id="outlined-required"
+            label="Username"
+            onChange={handleUsernameChange}
+          />
+          <TextField
+            required
+            id="outlined-required"
+            label="Password"
+            type='password'
+            onChange={handlePasswordChange}
+          />
+          <TextField
+            required
+            id="outlined-required"
+            label="First Name"
+            onChange={handleFirstnameChange}
+          />
+          <TextField
+            required
+            id="outlined-required"
+            label="Last Name"
+            onChange={handleLastnameChange}
+          />
+          <Button 
+            variant="contained"
+            color="primary"
+            disabled={!allFieldsFilled}
+            onClick={handleSignUp}
+          >
+            Sign Up
+          </Button>
+        </Stack>
+      </div>
+  );
+}
