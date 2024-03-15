19b5630f0b071b9352fb2f74603af4161b4c960d
John Elliott <johnelliott626@gmail.com>
2024-03-15 18:25:36 -0400

    delete after refactoring

--- API/src/routes/logout.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { logout } = require('../controllers/logoutController');
-const authenticateSession = require('../middleware/authenticateSession');
-
-
-router.post('/',authenticateSession, logout)
-
-module.exports = router;
