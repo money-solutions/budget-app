9de5457e3d376b8e1345d8ad21a34af93abbc7b4
John Elliott <johnelliott626@gmail.com>
2024-03-15 20:13:45 -0400

    it works

--- API/src/services/categoryService.js
+++ API/src/services/categoryService.js
@@ -1,14 +1,14 @@
 const queryDB = require("./db");
 
-async function createCategory(userID, budgetID, categoryName, categoryType, budgetAmount) {
-    const query = "INSERT INTO Categories (UserID, BudgetID, CategoryName, CategoryType, BudgetAmount) VALUES ($1, $2, $3, $4, $5)";
-    const { rowCount } = await queryDB(query, [userID, budgetID, categoryName, categoryType, budgetAmount]);
+async function createCategory(budgetID, categoryName, categoryType, budgetAmount) {
+    const query = "INSERT INTO Categories (BudgetID, CategoryName, CategoryType, BudgetAmount) VALUES ($1, $2, $3, $4)";
+    const { rowCount } = await queryDB(query, [budgetID, categoryName, categoryType, budgetAmount]);
     return rowCount === 1;
 }
 
-async function isCategoryUnique(userID, budgetID, categotyName) {
-    const query = "SELECT * FROM Budgets WHERE UserID = $1 AND BudgetID = $2 AND CategoryName = $3";
-    const { rows } = await queryDB(query, [userID, budgetID, categotyName]);
+async function isCategoryUnique(budgetID, categotyName) {
+    const query = "SELECT * FROM Categories WHERE BudgetID = $1 AND CategoryName = $2";
+    const { rows } = await queryDB(query, [budgetID, categotyName]);
     return rows.length === 0;
 }
 
---
+++ API/test/endpoints/category/categoryCreate.test.mjs
@@ -0,0 +1,29 @@
+export default async function categoryCreateTest(agent, expect) {
+    describe("Create Category Endpoint Test:", () => {
+        it("should return status 200 and a success message", async () => {
+            const res = await agent.post("/api/category").send({
+                budgetYear: 2020,
+                budgetMonths: [1, 2, 3, 4, 5, 6, 7, 8],
+                categoryName: "Test Category",
+                categoryType: "Income",
+                budgetAmount: 5000,
+            });
+
+            expect(res).to.have.status(200);
+            expect(res.body).to.have.property("message").to.equal("Categories successfully created.");
+        });
+
+        it("should return status 400 and a error message", async () => {
+            const res = await agent.post("/api/category").send({
+                budgetYear: 2021,
+                budgetMonths: [1, 2, 3, 4, 5, 6, 7, 8],
+                categoryName: "Test Category",
+                categoryType: "Income",
+                budgetAmount: 5000,
+            });
+
+            expect(res).to.have.status(400);
+            expect(res.body).to.have.property("message").to.equal("No Budgets not found.");
+        });
+    });
+}
