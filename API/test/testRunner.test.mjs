f4b6bb44d8a33397e8aaa53577e8970789041959
John Elliott <johnelliott626@gmail.com>
2024-03-29 16:54:17 -0400

    tests for accounts endpoint

--- API/test/testRunner.test.mjs
+++ API/test/testRunner.test.mjs
@@ -9,6 +9,8 @@ import userLogoutTest from "./endpoints/user/userLogout.test.mjs";
 import userDeleteTest from "./endpoints/user/userDelete.test.mjs";
 import budgetCreateTest from "./endpoints/budget/budgetCreate.test.mjs";
 import categoryCreateTest from "./endpoints/category/categoryCreate.test.mjs";
+import accountCreateTest from "./endpoints/account/accountCreate.test.mjs";
+import accountGetTest from "./endpoints/account/accountGet.test.mjs";
 
 const chai = use(chaiHttp);
 const agent = chai.request.agent(app);
@@ -23,6 +25,8 @@ describe("Tests Before User Session:", async () => {
     // All other tests should go here
     await budgetCreateTest(agent, expect);
     await categoryCreateTest(agent, expect);
+    await accountCreateTest(agent, expect);
+    await accountGetTest(agent, expect);
 
     // Delete the test user and all its entities and end the session
     await userDeleteTest(agent, expect);
